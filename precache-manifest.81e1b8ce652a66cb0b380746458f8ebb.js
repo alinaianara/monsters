25d4faee5a899c19edbddcf7c67cc0ac7a60e9fa
Alina <49275467+alinaianara@users.noreply.github.com>
2019-07-30 22:16:57 +0200

    Updates

---
+++ precache-manifest.81e1b8ce652a66cb0b380746458f8ebb.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "44719cc68992c1d1f2a10266d205ab7b",
+    "url": "/monsters/index.html"
+  },
+  {
+    "revision": "3fc23a0fc11eeb2cbc01",
+    "url": "/monsters/static/css/main.91952cb5.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "3fc23a0fc11eeb2cbc01",
+    "url": "/monsters/static/js/main.1c7804e9.chunk.js"
+  },
+  {
+    "revision": "0b202f8c0ed788dad152",
+    "url": "/monsters/static/js/runtime~main.39cb6628.js"
+  }
+]);
